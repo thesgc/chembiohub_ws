5a9e48300f7f2d3255e5e610419a6f33018ee06a
Strets123 <strets123@gmail.com>
2014-12-18 14:58:34 +0000

    fabfile

---
+++ fabfile.py
@@ -0,0 +1,81 @@
+from __future__ import with_statement
+from fabric.api import local
+from fabric.api import *
+from fabric.contrib.console import confirm
+import os
+env.hosts = ['staging.chembiohub.ox.ac.uk']
+
+def _deploy(code_dir, process_name):
+    # with settings(warn_only=True):
+    #     if run("test -d %s" % code_dir).failed:
+    #         run("git clone user@vcshost:/path/to/repo/.git %s" % code_dir)
+    with cd(code_dir):          
+        sudo("git pull --recurse-submodules", user="chembiohub")        
+        sudo("supervisorctl reload")
+        sudo("service apache2 reload reload")
+        with cd("src/ng-chem"):
+            run("bower install") 
+        sudo("source /home/chembiohub/.bashrc &&  source /home/chembiohub/anaconda/bin/activate chembiohub_ws && python manage.py syncdb --migrate && python manage.py collectstatic", user="chembiohub") 
+
+
+
+def prod():
+    # env.hosts = ['chembiohub.ox.ac.uk']
+    _deploy("/var/www/chembiocrunch/", "chem_bio_crunch")
+
+
+def stage():
+    
+    _deploy("/var/www/chembiohub_ws", "chem_bio_hub_ws")
+
+
+dirs = ["cbh_chembl_model_extension",
+        "cbh_chembl_ws_extension",
+        "chembl_beaker",
+        "chembl_business_model",
+        "chembl_core_db",
+        "chembl_core_model",
+        "chembl_extras",
+        "chembl_webservices",
+        "standardiser",
+        "ng-chem"
+        ]
+
+
+def prep():
+    dirnow = os.getcwd()
+    for directory in dirs:
+        with lcd(dirnow + "/src/" + directory):
+            try:
+                local("git add  .")
+            except:
+                pass
+            try:
+                local("git commit -a")
+            except:
+                pass
+            try:
+                local("git pull")
+            except:
+                pass
+            try: 
+                local("git push")
+            except:
+                pass
+    try:
+        local("git add .")
+    except:
+        pass
+    try:
+        local("git commit -a")
+    except:
+        pass
+    try:
+        local("git pull")
+    except:
+        pass
+    try: 
+        local("git push")
+    except:
+        pass
+
