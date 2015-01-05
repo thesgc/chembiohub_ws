470189cf77e8ff5e8152db998c3de2b4713452d0
paulbarrett79 <paulbarrett79@gmail.com>
2015-01-05 11:17:25 +0000

    setup config changes

--- chemblwsprovision.sh
+++ chemblwsprovision.sh
@@ -5,42 +5,55 @@ unzip indigo-python-1.1.11-linux.zip
 
 rm indigo-python-1.1.11-linux.zip
 
-echo "export PYTHONPATH=:/var/www/chembiohub_ws:/home/chembiohub/indigo-python-1.1.11-linux:/home/chembiohub/Tools/openbabel-install/lib" >> ~/.bashrc 
+echo "" >> ~/.bashrc 
 echo 'export DJANGO_SETTINGS_MODULE="deployment.settings.staging"' >> ~/.bashrc
 
 source ~/miniconda/bin/activate newbeak
 pip install django-cors-headers
 cd ~/chembiohub_ws/src/chembl_core_db
+python setup.py install
 python setup.py develop
 
 cd ../chembl_core_model/
-
+python setup.py install
 python setup.py develop
 
 cd ../chembl_webservices/
-
+python setup.py install
 python setup.py develop
 
 cd ../chembl_business_model/
-
+python setup.py install
 python setup.py develop
 
 cd ../standardiser/
-
+python setup.py install
 python setup.py develop
 
 cd ../chembl_beaker/
-
+python setup.py install
 python setup.py develop
 
 cd ../cbh_chembl_model_extension/
-
+python setup.py install
 python setup.py develop
 
 cd ../cbh_chembl_ws_extension/
-
+python setup.py install
 python setup.py develop
 
 cd ../chembl_extras/
-
+python setup.py install
 python setup.py develop
+
+sudo apt-get install nodejs -y
+
+sudo apt-get install npm -y
+
+sudo npm install -g bower
+
+sudo apt-get install nodejs-legacy -y
+
+cd /home/vagrant/chembiohub_ws/src/ng-chem
+
+bower install
