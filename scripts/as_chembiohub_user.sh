1b5d899ca48ae42139b04de60e2c9db5f82f27e2
strets123 <strets123@gmail.com>
2015-02-19 15:38:42 +0000

    boost not v

--- scripts/as_chembiohub_user.sh
+++ scripts/as_chembiohub_user.sh
@@ -1,7 +1,7 @@
 cd ~
 mkdir boost
 wget -O boost_1_56_0.tar.gz http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.gz/download
-tar xzvf boost_1_56_0.tar.gz
+tar xzf boost_1_56_0.tar.gz
 cd boost_1_56_0/
 ./bootstrap.sh --with-libraries=python,regex --prefix=/home/chembiohub/boost
 ./bjam install
