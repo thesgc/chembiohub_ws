8347e879f3684eb0974bd36ba593ba1cdb511680
strets123 <strets123@gmail.com>
2015-02-19 11:27:25 +0000

    submods + scripts dir change

---
+++ scripts/as_chembiohub_user.sh
@@ -0,0 +1,113 @@
+cd ~
+###Now Install the RDKit globally in order to make the database work
+  export RDKIT_SOURCE_ROOT=$HOME/rdkit
+
+wget http://sourceforge.net/projects/rdkit/files/rdkit/Q3_2014/RDKit_2014_09_2.tar.gz
+tar -xvf RDKit_2014_09_2.tar.gz
+mv rdkit-Release_2014_09_2 rdkit 
+
+export RDBASE=$HOME/rdkit
+export LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$RDBASE:$PYTHONPATH
+cd rdkit
+mkdir build
+cd build
+cmake -DRDK_BUILD_INCHI_SUPPORT=ON -DBOOST_ROOT=/usr/include ..
+make -j4 installcheck
+
+###Bower and node
+
+  
+###Now install openbabel and indigo and add them to python path
+
+  cd ~
+  
+  wget http://sourceforge.net/projects/openbabel/files/openbabel/2.3.2/openbabel-2.3.2.tar.gz
+  
+  tar -xvf openbabel-2.3.2.tar.gz
+  
+  cd openbabel-2.3.2
+  
+  mkdir build
+  
+  cd build
+  
+  cmake .. -DPYTHON_BINDINGS=ON -DCMAKE_INSTALL_PREFIX=~/Tools/openbabel-install
+  
+  #compile with 8 threads for speed
+  
+  make -j8
+  
+  make -j8 install
+  
+###Indigo like this:
+
+cd ~
+  wget https://dl.dropboxusercontent.com/u/10967207/indigo-python-1.1.11-linux.zip
+
+  unzip indigo-python-1.1.11-linux.zip
+
+  rm indigo-python-1.1.11-linux.zip
+
+
+  echo "export PYTHONPATH=:/var/www/chembiohub_ws:/home/chembiohub/indigo-python-1.1.11-linux:/home/chembiohub/Tools/openbabel-install/lib"  >> ~/.bashrc 
+  
+  echo 'export DJANGO_SETTINGS_MODULE="deployment.settings.staging"'  >> ~/.bashrc 
+
+###Inchi binaries like this:
+
+  cd ~/Tools
+  
+  wget http://www.iupac.org/fileadmin/user_upload/publications/e-resources/inchi/1.03/INCHI-1-BIN.zip
+  
+  unzip INCHI-1-BIN.zip
+  
+  gunzip INCHI-1-BIN/linux/64bit/inchi-1.gz
+  
+  chmod +x INCHI-1-BIN/linux/64bit/inchi-1
+
+
+###Install anaconda locally:
+
+  cd ~
+  rm anaconda_requirements.txt pip_requirements.txt
+
+wget https://raw.githubusercontent.com/thesgc/chembiohub_ws/master/anaconda_requirements.txt
+wget https://raw.githubusercontent.com/thesgc/chembiohub_ws/master/pip_requirements.txt
+  wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh -O anaconda.sh
+  
+  chmod +x anaconda.sh
+  
+  bash anaconda.sh -b
+  
+###Then change to that directory and add channels
+
+  cd anaconda/bin
+  
+  ./conda config --add channels https://conda.binstar.org/auto
+  
+  ./conda config --add channels https://conda.binstar.org/auto
+  
+  ./conda config --add channels https://conda.binstar.org/bcbio
+  
+  ./conda config --add channels https://conda.binstar.org/ric
+  
+  ./conda config --add channels https://conda.binstar.org/minadyn
+  
+  ./conda config --add channels https://conda.binstar.org/pkgw
+  
+  ./conda config --add channels https://conda.binstar.org/jacksongs
+  
+  ./conda config --add channels https://conda.binstar.org/mutirri
+  
+  ./conda config --add channels https://conda.binstar.org/zero323 
+    
+###Now create a virtualenv using the conda requirements file
+
+  ./conda create --yes python=2.7.6 -m -n chembiohub_ws --file=../../anaconda_requirements.txt
+source activate chembiohub_ws
+pip install -r ../../pip_requirements.txt
+
+
+
+
---
+++ scripts/as_sudoable_user.sh
@@ -0,0 +1,61 @@
+
+
+#!/bin/bash
+set -e
+#In order to install chembiohub web services and all chembl dependencies on anaconda, run the following:
+#===============================
+###First create the user that will run all the code
+
+
+
+cd ~
+###Now install all of the dependency apt gets in the environment
+
+  wget https://raw.githubusercontent.com/chembl/mychembl/master/install_core_libs.sh
+
+  sed "s/gem install gist//g" install_core_libs.sh >> install_core_libs1.sh
+ # sh install_core_libs1.sh
+
+###Now add a user for the install
+POSTGRES_COMMAND = "psql template1 -c 'DROP ROLE IF EXISTS $USER; create user $USER with superuser; CREATE EXTENSION IF NOT EXISTS hstore'"
+ sudo su postgres -c $POSTGRES_COMMAND
+  
+  
+###edit pg_hba.conf and add a line for your user 
+ECHO_COMMAND = 'echo "local all $USER ident" >> /etc/postgresql/9.3/main/pg_hba.conf'
+  sudo su postgres -c $ECHO_COMMAND
+
+  sudo service postgresql restart
+  
+   sudo apt-get install -y apache2 
+sudo apt-get install -y libapache2-mod-proxy-html libxml2-devsudo apt-get install libxml2-dev
+sudo a2enmod proxy proxy_http headers
+  sudo apt-get install -y supervisor
+
+  sudo apt-get install -y nodejs
+
+  sudo apt-get install -y npm
+
+  sudo npm install -g bower
+
+  sudo apt-get install -y nodejs-legacy
+
+cd ~
+wget http://bitbucket.org/eigen/eigen/get/2.0.15.tar.bz2
+tar xvf 2.0.15.tar.bz2
+cd eigen-eigen-0938af7840b0/; mkdir build; cd build; cmake ..
+sudo make install  # Does the job.
+cd ../..
+sudo apt-get install pkg-config
+
+id -u chembiohub &>/dev/null ||   sudo useradd -G www-data -s /bin/bash -m chembiohub
+   
+sudo su chembiohub -c chembiohub.sh
+
+DROP_COMMAND = "psql template1 -c 'DROP ROLE IF EXISTS $USER; '"
+
+  sudo su postgres -c $DROP_COMMAND
+
+
+
+
