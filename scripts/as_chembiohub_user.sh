28adbf9626852e275ad0ea4d29bdf9f3117deeb5
strets123 <strets123@gmail.com>
2015-02-21 14:25:46 +0000

    Update as_chembiohub_user.sh

--- scripts/as_chembiohub_user.sh
+++ scripts/as_chembiohub_user.sh
@@ -4,8 +4,7 @@ cd ~
 ###Now Install the RDKit globally in order to make the database work
   export RDKIT_SOURCE_ROOT=$HOME/rdkit
 
-wget http://sourceforge.net/projects/rdkit/files/rdkit/Q3_2014/RDKit_2014_09_2.tar.gz
-tar -xvf RDKit_2014_09_2.tar.gz
+tar -xvf /tmp/RDKit_2014_09_2.tar.gz
 mv rdkit-Release_2014_09_2 rdkit 
 export RDBASE=$HOME/rdkit
 export LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
@@ -30,11 +29,10 @@ cd $RDBASE/Code/PgSQL/rdkit
 make
 
 cd ~
-  wget http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh -O anaconda.sh
   
-  chmod +x anaconda.sh
+  #chmod +x /tmp/anaconda.sh
   
-  bash anaconda.sh -b
+  bash /tmp/anaconda.sh -b
 
 
 
