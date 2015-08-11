2c82e02f2c0c66f9edb3daab3a2aa162643d181d
strets123 <strets123@gmail.com>
2015-08-11 20:59:17 +0100

    Create vagrant_provision.sh

---
+++ scripts/vagrant_provision.sh
@@ -0,0 +1,11 @@
+echo "create user chembl with password 'chembl'; create database cbh_chembl ; grant all privileges on database cbh_chembl to chembl;" > /tmp/creator
+
+sudo su postgres -c "psql < /tmp/creator"
+
+echo 'export DJANGO_SETTINGS_MODULE="deployment.settings.development"' > ~/.bashrc
+
+source ~/.bashrc
+
+  mkdir ~/.local/lib/python2.7/site-packages -p
+  cd ~/.local/lib/python2.7/site-packages/
+  wget https://raw.githubusercontent.com/thesgc/chembiohub_ws/master/scripts/vagrant_pythonpath.pth
