cd48d5d14102ed8f87a6e5e29ad890b38998952c
strets123 <strets123@gmail.com>
2015-08-11 21:04:17 +0100

    Update vagrant_provision.sh

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -1,7 +1,7 @@
 echo "create user chembl with password 'chembl'; create database cbh_chembl ; grant all privileges on database cbh_chembl to chembl;" > /tmp/creator
 
 sudo su postgres -c "psql < /tmp/creator"
-
+sudo su postgres -c "echo 'create extension if not exists hstore;create extension if not exists rdkit;'"
 echo 'export DJANGO_SETTINGS_MODULE="deployment.settings.development"' > ~/.bashrc
 
 source ~/.bashrc
