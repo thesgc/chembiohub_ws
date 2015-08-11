5e5a0d9186faebba155cd553f57ae1c9c73f3928
strets123 <strets123@gmail.com>
2015-08-11 16:09:20 +0100

    Update vagrant_mychembl.sh

--- scripts/vagrant_mychembl.sh
+++ scripts/vagrant_mychembl.sh
@@ -1,6 +1,7 @@
  cd ~
  
   rm anaconda_requirements.txt pip_requirements.txt
+wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
 
 wget https://raw.githubusercontent.com/thesgc/chembiohub_ws/master/anaconda_requirements.txt
 wget https://raw.githubusercontent.com/thesgc/chembiohub_ws/master/pip_requirements.txt
