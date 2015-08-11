34a439005d56133e13423111a94430c5fe7a653a
strets123 <strets123@gmail.com>
2015-08-11 15:40:14 +0100

    Create vagrant_mychembl.sh

---
+++ scripts/vagrant_mychembl.sh
@@ -0,0 +1,34 @@
+ cd ~
+  rm anaconda_requirements.txt pip_requirements.txt
+
+wget https://raw.githubusercontent.com/thesgc/chembiohub_ws/master/anaconda_requirements.txt
+wget https://raw.githubusercontent.com/thesgc/chembiohub_ws/master/pip_requirements.txt
+
+  
+###Then change to that directory and add channels
+
+  cd anaconda/bin
+  
+  ./conda config --add channels https://conda.binstar.org/auto
+  
+  ./conda config --add channels https://conda.binstar.org/auto
+  
+  ./conda config --add channels https://conda.binstar.org/bcbio
+  
+  ./conda config --add channels https://conda.binstar.org/ric
+  
+  ./conda config --add channels https://conda.binstar.org/minadyn
+  
+  ./conda config --add channels https://conda.binstar.org/pkgw
+  
+  ./conda config --add channels https://conda.binstar.org/jacksongs
+  
+  ./conda config --add channels https://conda.binstar.org/mutirri
+  
+  ./conda config --add channels https://conda.binstar.org/zero323 
+    
+###Now create a virtualenv using the conda requirements file
+
+  ./conda create --yes -m -n chembiohub_ws --file=../../anaconda_requirements.txt
+source activate chembiohub_ws
+pip install -r ../../pip_requirements.txt
