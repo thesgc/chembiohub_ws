cc33d2b4642b105e4ada56098a418e57d4504120
Strets123 <strets123@gmail.com>
2015-09-02 08:53:35 +0100

    prov

--- scripts/vagrant_prov.sh
+++ scripts/vagrant_prov.sh
@@ -1,7 +1,7 @@
 cd /home/vagrant/chembiohub_ws/src/ng-chem
 
-sudo -H -u vagrant bash -c npm install
-sudo -H -u vagrant bash -c  bower install
+su - vagrant -c "npm install"
+su - vagrant -c  "bower install"
 
 
  sudo npm install -g yo generator-angular generator-karma
