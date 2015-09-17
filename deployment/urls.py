a7e26bac502d30a6a119ce8edb541ea24edbd978
Strets123 <strets123@gmail.com>
2015-09-17 09:16:54 +0100

    deploy

--- deployment/urls.py
+++ deployment/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 
-urlpatterns = patterns('',
-
-   (r'^', include('cbh_chembl_ws_extension.urls')),
+patt = ('',(r'^', include('cbh_chembl_ws_extension.urls')),
    (r'^', include('cbh_datastore_ws.urls')),
-   (r'^', include('tastypie_spore_docs.urls')),
+   
     (r'^django-rq/', include('django_rq.urls')),
+)
 
+if "tastypie_spore_docs" in settings.INSTALLED_APPS:
+    patt += ((r'^', include('tastypie_spore_docs.urls'))),
+
+urlpatterns = patterns(
+
+    *patt
     )
-
