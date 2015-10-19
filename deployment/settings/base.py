c5fa40b50f62bf1662e353873d042cdf0b3454e9
Andrew Stretton <strets123@gmail.com>
2015-10-19 14:00:42 +0200

    tweaks

--- deployment/settings/base.py
+++ deployment/settings/base.py
@@ -164,6 +164,7 @@ TEMPLATE_DIRS = (
 # Don't forget to use absolute paths, not relative paths.
 )
 # GEOMETRY_BACKEND = ''
+
 GDAL_LIBRARY_PATH = '/home/vagrant/miniconda/bin/chembiohub_ws/lib/libgdal.so' 
 GEOS_LIBRARY_PATH = '/home/vagrant/miniconda/bin/chembiohub_ws/lib/libgeos_c.so'
 INSTALLED_APPS = (
