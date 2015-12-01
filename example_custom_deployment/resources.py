a90dc00a454edf7ba890cbc8216c708a0b2807f1
Strets123 <strets123@gmail.com>
2015-12-01 14:13:42 +0000

    resources for custom app

--- example_custom_deployment/resources.py
+++ example_custom_deployment/resources.py
@@ -1,13 +1,37 @@
 from cbh_chembl_ws_extension.compounds import CBHCompoundBatchResource
 from django.http import HttpRequest
+from cbh_chembl_ws_extension.parser import get_uncurated_fields_from_file
+
+
+def add_external_ids_to_file_object(python_file_obj):
+    """Save the contents of the sdf file to the extenal system and fill in the external ID field
+    Write the upadted file object to disc"""
+    pass
+
+def validate_file_object_externally(python_file_obj):
+    """E.G. take the sdf file and set a status field to say whether it is in the external system
+    Write the upadted file object to disc"""
+    pass
+
 
 class AlteredCompoundBatchResource(CBHCompoundBatchResource):
-    def validate_multi_batch(self, multiple_batch, bundle, request, batches):
-        response = super(AlteredCompoundBatchResource, self).validate_multi_batch(multiple_batch, bundle, request, batches)
-        
-        request.GET = request.GET.copy()
-        request.GET["format"] = "sdf"
-        request.GET["current_batch"] = multiple_batch.id
-        sdf_file_resp = super(AlteredCompoundBatchResource, self).get_part_processed_multiple_batch(request)
-        print sdf_file_resp.__dict__
-        return response
+
+
+    def alter_batch_data_after_save(self, batch_list, python_file_obj):
+        """Take the original sdf file and run an external process with it such that new data can be written across 
+        after save of the data into ChemBioHub"""
+        add_ids_to_file_object(python_file_obj)
+        fielderrors = {}
+        fielderrors["stringdate"] = set([])
+        fielderrors["number"] = set([])
+        fielderrors["integer"] = set([])
+        uncurated_data = get_uncurated_fields_from_file(file_obj)
+        for index, batch in enumerate(batch_list):
+            #This assumes project is set up with exactly right custom fields
+            batch.custom_fields = uncurated_data[index]
+            batch.save()
+
+
+    def preprocess_sdf_file(self, python_file_obj):
+        """Hook to preprocess an sdf file - assumes that the file will be written to disc"""
+        validate_file_object_externally(python_file_obj)
