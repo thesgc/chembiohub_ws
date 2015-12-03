a421e729347caee62b136c911fe20aa27d7f17aa
Strets123 <strets123@gmail.com>
2015-12-03 14:50:48 +0000

    settings

--- deployment/settings/andy.py
+++ deployment/settings/andy.py
@@ -3,3 +3,7 @@ from .default import *
 WEBSERVICES_NAME='dev'
 LOGIN_REDIRECT_URL = "http://localhost:9000/#/projects/list"
 LOGOUT_REDIRECT_URL = "login"
+
+
+SESSION_COOKIE_NAME = 'dev_sessionid'
+CSRF_COOKIE_NAME = 'devcsrftoken'
