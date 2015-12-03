48368a43d4fd6c9a73d93a422c9f89bc14c00d8a
Strets123 <strets123@gmail.com>
2015-12-03 15:06:43 +0000

    fixes for scarab

--- deployment/settings/andy.py
+++ deployment/settings/andy.py
@@ -1,4 +1,117 @@
-from .default import *
+from .base import *
+DEBUG=True
+TEMPLATE_DEBUG = DEBUG
+
+import os
+import pwd
+
+def get_username():
+    return pwd.getpwuid( os.getuid() )[ 0 ]
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'chembiohub_db', # Or path to database file if using sqlite3.
+        'USER': get_username(), # Not used with sqlite3.
+        'PASSWORD': '', # Not used witis oracle
+        'HOST': '/home/chembiohub/postgressocket/', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+    },
+}
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+import os
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+RQ_QUEUES = {
+    'default': {
+        'USE_REDIS_CACHE': 'default',
+    },
+}
+
+
+
+
+SESSION_COOKIE_NAME = 'chembiohub_sessionid'
+CSRF_COOKIE_NAME = 'chembiohubcsrftoken'
+STATIC_ROOT = '/srv/chembiohub/chembiohub_ws/deployment/static'
+MEDIA_ROOT = '/home/chembiohub/media/'
+FLOWJS_PATH = MEDIA_ROOT + 'flow'
+
+
+LOGIN_REDIRECT_URL = '/chembiohub/#/projects/list'
+LOGIN_URL = '/chembiohub/login'
+LOGOUT_REDIRECT_URL = '/chembiohub/login'
+WEBSERVICES_NAME='chembiohub/api'
+
+INCHI_BINARIES_LOCATION = {"1.02" :"/srv/chembiohub/INCHI-1-BIN/linux/64bit/inchi-1"}
+
+SESSION_CACHE_ALIAS= 'chembiohub'
+CACHES = {
+"default": {
+            "BACKEND": "django_redis.cache.RedisCache",
+                                "LOCATION": "redis://127.0.0.1:6379/1",
+                                                            "OPTIONS": {
+                                                                                                    "CLIENT_CLASS": "django_redis.client.DefaultClient",
+                                                                                                                                                    }
+                                                                                                                                                },
+
+
+
+
+"chembiohub": {
+            "BACKEND": "django_redis.cache.RedisCache",
+                    "LOCATION": "redis://127.0.0.1:6379/1",
+                            "OPTIONS": {
+                                        "CLIENT_CLASS": "django_redis.client.DefaultClient",
+                                                }
+        }
+}
+
+ES_PREFIX = 'chembiohub'
+
+
+ 
+STATIC_URL = '/chembiohub/static/'
+
+STATICFILES_DIRS = (
+'/srv/chembiohub/chembiohub_ws/src/ng-chem/dist',
+)
+#Add a template dir so that the html content of index.html can be brought in as a static template when in production so login is handled by Django - see base.py in cbh_chembl_ws_extension (Index() view)
+TEMPLATE_DIRS = (
+'/srv/chembiohub/chembiohub_ws/src/ng-chem',
+)
+
+
 
 WEBSERVICES_NAME='dev'
 LOGIN_REDIRECT_URL = "http://localhost:9000/#/projects/list"
--- example_custom_deployment/resources.py
+++ example_custom_deployment/resources.py
@@ -20,12 +20,12 @@ class AlteredCompoundBatchResource(CBHCompoundBatchResource):
     def alter_batch_data_after_save(self, batch_list, python_file_obj):
         """Take the original sdf file and run an external process with it such that new data can be written across 
         after save of the data into ChemBioHub"""
-        add_ids_to_file_object(python_file_obj)
+        add_external_ids_to_file_object(python_file_obj)
         fielderrors = {}
         fielderrors["stringdate"] = set([])
         fielderrors["number"] = set([])
         fielderrors["integer"] = set([])
-        uncurated_data = get_uncurated_fields_from_file(file_obj)
+        uncurated_data = get_uncurated_fields_from_file(python_file_obj, fielderrors)
         for index, batch in enumerate(batch_list):
             #This assumes project is set up with exactly right custom fields
             batch.custom_fields = uncurated_data[index]
