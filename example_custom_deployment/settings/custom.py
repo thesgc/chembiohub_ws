99c788ebe0ab79711b6262d4558bec6ef277e660
Strets123 <strets123@gmail.com>
2015-12-04 13:14:37 +0000

    custom settings

--- example_custom_deployment/settings/custom.py
+++ example_custom_deployment/settings/custom.py
@@ -1,3 +1,9 @@
 from deployment.settings.default import *
 
 ROOT_URLCONF = 'example_custom_deployment.urls'
+
+LOGGING["loggers"]["example_custom_deployment"] = {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+            }
