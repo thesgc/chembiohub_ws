182424f4ee448b9997d778f3803d5296b03e12c1
Strets123 <strets123@gmail.com>
2015-12-05 14:49:19 +0000

    adding the user authentication for custom app

--- example_custom_deployment/settings/custom.py
+++ example_custom_deployment/settings/custom.py
@@ -1,3 +1,8 @@
 from deployment.settings.test import *
 
 ROOT_URLCONF = 'example_custom_deployment.urls'
+
+#Note that there is no standard password login possible
+AUTHENTICATION_BACKENDS = ('example_custom_deployment.backends.CustomAuthBackend',)
+
+ADMIN_USERS = ('bmarsden','ddamerell',)
