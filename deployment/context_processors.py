7147d4be2c79bf8ebc3318405dd57aa711d869b6
Strets123 <strets123@gmail.com>
2015-12-10 10:30:24 +0000

    tweaks to settings

--- deployment/context_processors.py
+++ deployment/context_processors.py
@@ -1,8 +1,7 @@
 from django.conf import settings
 
 def static_paths(request):
-    print settings.ENV_NAME
-    # if settings.ENV_NAME  != "dev":
-    #     return {"site_prefix" : "/%s/" % settings.ENV_NAME}
-    # else:
-    return {"site_prefix" : "/../" }
+    if settings.ENV_NAME  != "dev":
+        return {"site_prefix" : "/%s/" % settings.ENV_NAME}
+    else:
+        return {"site_prefix" : "/../" }
