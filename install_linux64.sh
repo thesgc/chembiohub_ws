0cf7aaadfe8b0588b09f8123fb6d15b739a35ed4
Strets123 <strets123@gmail.com>
2015-12-10 12:21:23 +0000

    install ubuntu

--- install_linux64.sh
+++ install_linux64.sh
@@ -2,8 +2,8 @@
 ENV_NAME=$1
 
 if ! hash conda 2>/dev/null; then
-    cd ~
-    wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.0-Linux-x86_64.sh
+    
+    wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.0-Linux-x86_64.sh 
     bash Anaconda2-2.4.0-Linux-x86_64.sh -b    
     rm Anaconda2-2.4.0-Linux-x86_64.sh
      echo "export PATH=$HOME/anaconda2/bin/:\$PATH" >> ~/.bashrc
