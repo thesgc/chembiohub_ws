f1906686e70dd322f678e5d2532908df78f0bd5d
strets123 <strets123@gmail.com>
2016-01-16 14:56:45 +0000

    tidying bash if statement

--- install_linux64.sh
+++ install_linux64.sh
@@ -3,7 +3,7 @@ ENV_NAME=$1
 OLD_PATH="$PATH"
 
 
-if [! hash conda 2>/dev/null]
+if [-x "$(command -v conda)"]
 then
     wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.0-Linux-x86_64.sh 
     if [$RD_BASE = "/home/chembl/rdkit"]
