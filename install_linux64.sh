c622b4347563fc8b257411c8b60048f89bb529c1
strets123 <strets123@gmail.com>
2016-01-16 11:58:26 +0000

    Update install_linux64.sh

--- install_linux64.sh
+++ install_linux64.sh
@@ -3,15 +3,15 @@ ENV_NAME=$1
 OLD_PATH="$PATH"
 
 
-if ! hash conda 2>/dev/null; then
-    
+if [! hash conda 2>/dev/null]
+then
     wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.0-Linux-x86_64.sh 
-if [$RD_BASE = "/home/chembl/rdkit"]: then
-    bash Anaconda2-2.4.0-Linux-x86_64.sh -b -p /srv/chembiohub/anaconda2
-
-else: then
-    bash Anaconda2-2.4.0-Linux-x86_64.sh -b    
-fi
+    if [$RD_BASE = "/home/chembl/rdkit"]
+    then
+        bash Anaconda2-2.4.0-Linux-x86_64.sh -b -p /srv/chembiohub/anaconda2
+    else
+        bash Anaconda2-2.4.0-Linux-x86_64.sh -b    
+    fi
     rm Anaconda2-2.4.0-Linux-x86_64.sh
      echo "export PATH=$HOME/anaconda2/bin/:\$PATH" >> ~/.bashrc
      #source ~/.bashrc
