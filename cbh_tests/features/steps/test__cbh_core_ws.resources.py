c6f2b91840d58ae3f86d08263bf3afc35a918d01
Strets123 <strets123@gmail.com>
2016-01-28 08:33:45 +0000

    making tests work for saved searches for thesgc/chembiohub_helpdesk#286

--- cbh_tests/features/steps/test__cbh_core_ws.resources.py
+++ cbh_tests/features/steps/test__cbh_core_ws.resources.py
@@ -52,3 +52,71 @@ def skinning(context):
     context.test_case.assertHttpOK(resp)
 
     
+
+
+
+
+
+@then(u'the default project type has an id of 3')
+def step_impl(context):
+    id = -1
+    for ptype in context.project_types:
+        if ptype["set_as_default"]:
+            id=ptype["id"]
+    context.test_case.assertEqual(id, 3)
+
+
+@when(u'I take the project type model with id 1 and set it to default')
+def step_impl(context):
+    ptype = ProjectType.objects.get(id=1)
+    ptype.set_as_default = True
+    ptype.save()
+
+
+@then(u'the previous default project type has been removed by the system and only the new one with id 1 remains')
+def step_impl(context):
+    qs = ProjectType.objects.filter(set_as_default=True)
+    context.test_case.assertEqual(qs.count(), 1)
+    context.test_case.assertEqual(qs[0].id, 1)
+
+
+@then(u'the project type with id 4 has the saved_search_project_type')
+def step_impl(context):
+    id = -1
+    for ptype in context.project_types:
+        if ptype["saved_search_project_type"]:
+            id=ptype["id"]
+    context.test_case.assertEqual(id, 4)
+
+@then(u'the project type with id 4 has a static project template to allow saved searches to be saved as compound batches')
+def step_impl(context):
+    project_template = None
+    for ptype in context.project_types:
+        if ptype["saved_search_project_type"]:
+            project_template = json.dumps(ptype["project_template"]["custom_field_config"]["project_data_fields"])
+    #This variable is a copy of the variable in ProjectType.SAVED_SEARCH_TEMPLATE, if you reimplement you must also change the test as an insurance policy for the front end code
+    STATIC_SHOUlD_BE_UNLESS_SYSTEM_CHANGED = [{
+                            "required": False,
+                            "field_type": "char",
+                            "open_or_restricted": "open",
+                            "name": "Alias"
+                        },
+                        {
+                            "required": False,
+                            "field_type": "char",
+                            "open_or_restricted": "open",
+                            "name": "URL"
+                        }]
+    context.test_case.assertEqual(project_template, json.dumps(STATIC_SHOUlD_BE_UNLESS_SYSTEM_CHANGED))
+
+
+
+
+
+
+
+
+
+
+
+
