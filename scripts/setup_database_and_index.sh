8693811b9a3fcd09db42d22e638194dbe30522c2
Strets123 <strets123@gmail.com>
2016-02-17 12:31:08 +0000

    secret key

--- scripts/setup_database_and_index.sh
+++ scripts/setup_database_and_index.sh
@@ -11,11 +11,14 @@ export PATH=$CONDA_ENV_PATH/bin:$OLD_PATH
 
 
 createdb -h $CONDA_ENV_PATH/var/postgressocket/ ${ENV_NAME}_db -T template1
-
+python generate_secret_settings.py > deployment/settings/secret.py
 python manage.py migrate
 python manage.py loaddata datatypes.json
 python manage.py loaddata projecttypes.json
 python manage.py reindex_compounds
 python manage.py reindex_datapoint_classifications
+cd src/ng-chem
+bower install
+cd ../..
 python manage.py collectstatic --noinput
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
