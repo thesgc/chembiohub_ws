dddd357eee7ed26fc43bc9a765841f3568a68cd6
Strets123 <strets123@gmail.com>
2016-02-17 11:09:52 +0000

    script tweaks

--- scripts/setup_database_and_index.sh
+++ scripts/setup_database_and_index.sh
@@ -1,12 +1,13 @@
 
  #Start postgres in foreground
 export ENV_NAME="$1"
-source activate "$ENV_NAME"
+OLD_PATH="$PATH"
 
-cd "$2"
 
-psql  -h $CONDA_ENV_PATH/var/postgressocket -c "create extension if not exists hstore;create extension if not exists  rdkit;" template1
+#source activate $ENV_NAME
 
+export CONDA_ENV_PATH=$(conda info | grep "envs dir" | cut -c 25-)/$ENV_NAME
+export PATH=$CONDA_ENV_PATH/bin:$OLD_PATH
 
 
 createdb -h $CONDA_ENV_PATH/var/postgressocket/ ${ENV_NAME}_db -T template1
@@ -18,3 +19,4 @@ python manage.py reindex_compounds
 python manage.py reindex_datapoint_classifications
 python manage.py createsuperuser
 python manage.py collectstatic
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
