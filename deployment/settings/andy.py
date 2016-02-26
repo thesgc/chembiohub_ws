4c72dbe9db94ba52d916bda72057e96d49a279ac
Strets123 <strets123@gmail.com>
2016-02-26 13:39:14 +0000

    v2 start

--- deployment/settings/andy.py
+++ deployment/settings/andy.py
@@ -1,137 +1,36 @@
-from .base import *
-DEBUG=True
-TEMPLATE_DEBUG = DEBUG
-
-import os
-import pwd
-
-def get_username():
-    return pwd.getpwuid( os.getuid() )[ 0 ]
+from .default import *
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'chembiohub_db', # Or path to database file if using sqlite3.
-        'USER': get_username(), # Not used with sqlite3.
-        'PASSWORD': '', # Not used witis oracle
-        'HOST': '/home/chembiohub/postgressocket/', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
-    },
-}
-ENV_NAME = "dev"
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
-# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-# EMAIL_FILE_PATH = '/tmp/app-messages' # change this to a proper location
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
-        },
-    },
-}
+INSTALLED_APPS = (
+      'django.contrib.auth',
 
 
-EMAIL_HOST = 'smtp.ox.ac.uk'
+       "django_hstore",
+
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+        'grappelli',
+    'django.contrib.admin',
+   'deployment',
+   'chembl_core_db',
+   'chembl_core_model',
+  'chembl_business_model', 
+         'cbh_core_api',
+
+       'flowjs',
+       'cbh_core_model',
+       'cbh_datastore_model',
+  'cbh_chembl_model_extension',    
+  'cbh_chem_api',
+  'cbh_chembl_id_generator',
+  'solo',
+    "django_rq",
+            'tastypie',
 
 
-# The email address to send on behalf of
-SERVER_EMAIL = 'andrew.stretton@sgc.ox.ac.uk'
+   )
 
-
-
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
-
-RQ_QUEUES = {
-    'default': {
-        'USE_REDIS_CACHE': 'default',
-    },
-}
-
-
-
-
-SESSION_COOKIE_NAME = 'dev_sessionid'
-CSRF_COOKIE_NAME = 'devcsrftoken'
-STATIC_ROOT = '/srv/chembiohub/chembiohub_ws/deployment/static'
-MEDIA_ROOT = '/home/chembiohub/media/'
-FLOWJS_PATH = MEDIA_ROOT + 'flow'
-
-
-LOGIN_REDIRECT_URL = '/chembiohub/#/projects/list'
-LOGIN_URL = '/dev/login'
-LOGOUT_REDIRECT_URL = '/dev/login'
-WEBSERVICES_NAME='dev/api'
-
-INCHI_BINARIES_LOCATION = {"1.02" :"/srv/chembiohub/INCHI-1-BIN/linux/64bit/inchi-1"}
-
-SESSION_CACHE_ALIAS= 'chembiohub'
-CACHES = {
-"default": {
-            "BACKEND": "django_redis.cache.RedisCache",
-                                "LOCATION": "redis://127.0.0.1:6379/1",
-                                                            "OPTIONS": {
-                                                                                                    "CLIENT_CLASS": "django_redis.client.DefaultClient",
-                                                                                                                                                    }
-                                                                                                                                                },
-
-
-
-
-"chembiohub": {
-            "BACKEND": "django_redis.cache.RedisCache",
-                    "LOCATION": "redis://127.0.0.1:6379/1",
-                            "OPTIONS": {
-                                        "CLIENT_CLASS": "django_redis.client.DefaultClient",
-                                                }
-        }
-}
-
-ES_PREFIX = 'chembiohub'
-
-
- 
-STATIC_URL = '/chembiohub/static/'
-
-STATICFILES_DIRS = (
-'/srv/chembiohub/chembiohub_ws/src/ng-chem/dist',
-)
-#Add a template dir so that the html content of index.html can be brought in as a static template when in production so login is handled by Django - see base.py in cbh_chembl_ws_extension (Index() view)
-TEMPLATE_DIRS = (
-'/srv/chembiohub/chembiohub_ws/src/ng-chem',
-)
-
-
-
-WEBSERVICES_NAME='dev'
-LOGIN_REDIRECT_URL = "http://localhost:9000/#/projects/list"
-LOGOUT_REDIRECT_URL = "login"
-
-
-SESSION_COOKIE_NAME = 'dev_sessionid'
-CSRF_COOKIE_NAME = 'devcsrftoken'
-
-DEBUG=True
-TEMPLATE_DEBUG=True
-
-
-FULL_RESPONSE_LOGGING = False
+ROOT_URLCONF = 'deployment.urls_v2'
---
+++ deployment/urls_v2.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+
+patt = ('',(r'^', include('cbh_chem_api.urls')),
+   
+    (r'^django-rq/', include('django_rq.urls')),
+)
+
+if "tastypie_spore_docs" in settings.INSTALLED_APPS:
+    patt += ((r'^', include('tastypie_spore_docs.urls'))),
+
+urlpatterns = patterns(
+
+    *patt
+    )
