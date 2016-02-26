9b91552520f6b6cecf3bbd222219cee5dd66d3d0
Strets123 <strets123@gmail.com>
2016-02-26 13:58:25 +0000

    scripts

--- scripts/setup_database_and_index.sh
+++ scripts/setup_database_and_index.sh
@@ -3,14 +3,15 @@ set -e
 export ENV_NAME="$1"
 OLD_PATH="$PATH"
 
-
+sudo service supervisor restart
+sleep 10
 #source activate $ENV_NAME
 
 export CONDA_ENV_PATH=/home/$2/anaconda2/envs/$ENV_NAME
 export PATH=$CONDA_ENV_PATH/bin:$OLD_PATH
 
 
-$CONDA_ENV_PATH/bin/psql  -h $CONDA_ENV_PATH/var/postgressocket -c "create extension if not exists hstore;create extension if not exists  rdkit;" template1
+$CONDA_ENV_PATH/bin/psql  -h $CONDA_ENV_PATH/var/postgressocket/ -c "create extension if not exists hstore;create extension if not exists  rdkit;" template1
 $CONDA_ENV_PATH/bin/createdb -h $CONDA_ENV_PATH/var/postgressocket/ ${ENV_NAME}_db -T template1
 
 $CONDA_ENV_PATH/bin/python generate_secret_settings.py > deployment/settings/secret.py
