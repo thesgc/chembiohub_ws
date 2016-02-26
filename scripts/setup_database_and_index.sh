8d7a53176ef7e9a00408ab7dbf20422fe72a0c2b
Strets123 <strets123@gmail.com>
2016-02-26 11:05:30 +0000

    setup

--- scripts/setup_database_and_index.sh
+++ scripts/setup_database_and_index.sh
@@ -10,17 +10,17 @@ export CONDA_ENV_PATH=/home/$2/anaconda2/envs/$ENV_NAME
 export PATH=$CONDA_ENV_PATH/bin:$OLD_PATH
 
 
-psql  -h $CONDA_ENV_PATH/var/postgressocket -c "create extension if not exists hstore;create extension if not exists  rdkit;" template1
-createdb -h $CONDA_ENV_PATH/var/postgressocket/ ${ENV_NAME}_db -T template1
+$CONDA_ENV_PATH/bin/psql  -h $CONDA_ENV_PATH/var/postgressocket -c "create extension if not exists hstore;create extension if not exists  rdkit;" template1
+$CONDA_ENV_PATH/bin/createdb -h $CONDA_ENV_PATH/var/postgressocket/ ${ENV_NAME}_db -T template1
 
-python generate_secret_settings.py > deployment/settings/secret.py
-python manage.py migrate
-python manage.py loaddata datatypes.json
-python manage.py loaddata projecttypes.json
-python manage.py reindex_compounds
-python manage.py reindex_datapoint_classifications
+$CONDA_ENV_PATH/bin/python generate_secret_settings.py > deployment/settings/secret.py
+$CONDA_ENV_PATH/bin/python manage.py migrate
+$CONDA_ENV_PATH/bin/python manage.py loaddata datatypes.json
+$CONDA_ENV_PATH/bin/python manage.py loaddata projecttypes.json
+$CONDA_ENV_PATH/bin/python manage.py reindex_compounds
+$CONDA_ENV_PATH/bin/python manage.py reindex_datapoint_classifications
 cd src/ng-chem
 bower install
 cd ../..
-python manage.py collectstatic --noinput
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
+$CONDA_ENV_PATH/bin/python manage.py collectstatic --noinput
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | $CONDA_ENV_PATH/bin/python manage.py shell
