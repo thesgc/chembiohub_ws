273389e93dddea6236a54a47042724d3d68d536a
paulbarrett79 <paulbarrett79@gmail.com>
2016-03-04 12:22:07 +0000

    FlowFile items for thesgc/chembiohub_helpdesk#930

--- deployment/settings/vagrant.py
+++ deployment/settings/vagrant.py
@@ -9,3 +9,37 @@ WEBSERVICES_NAME='dev'
 
 LOGIN_REDIRECT_URL = "http://localhost:9000/#/projects/list"
 LOGOUT_REDIRECT_URL = "login"
+
+INSTALLED_APPS = (
+      'django.contrib.auth',
+
+
+       "django_hstore",
+
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+        'grappelli',
+    'django.contrib.admin',
+   'deployment',
+   'chembl_core_db',
+   'chembl_core_model',
+  'chembl_business_model', 
+         'cbh_core_api',
+
+       'flowjs',
+       'cbh_core_model',
+       'cbh_datastore_model',
+  'cbh_chembl_model_extension',    
+  'cbh_chem_api',
+  'cbh_chembl_id_generator',
+  'solo',
+    "django_rq",
+            'tastypie',
+
+
+   )
+
+ROOT_URLCONF = 'deployment.urls_v2'
