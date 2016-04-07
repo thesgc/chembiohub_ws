661537591b34ff9d22a8257491fb58c2518903aa
Strets123 <strets123@gmail.com>
2016-04-07 11:00:11 +0100

    sort mapping

--- cbh_chem_api/tasks.py
+++ cbh_chem_api/tasks.py
@@ -13,9 +13,17 @@ def process_batch_list(batch_list):
 def get_structure_search_for_projects(project_ids, search_type, smiles):
     queryset = CompoundMols.objects.filter(molecule__project_id__in=project_ids)
     search_function = getattr(queryset, search_type)
-    batch_ids = search_function(smiles).values_list("molecule_id", flat=True)
+    mol_ids = search_function(smiles).order_by("-molecule_id").values_list("molecule_id", flat=True)
+    restricted = False
+    if search_type == "with_substructure":
+        mol_ids = mol_ids[:1000]
+        mol_ids = list(mol_ids)
+        if len(mol_ids) == 1000:
+            restricted = True
+            
     batch_ids = CBHCompoundBatch.objects.filter(related_molregno_id__in=batch_ids).values_list("id", flat=True)
-    return {"project_ids" : project_ids,"batch_ids" : batch_ids}
+
+    return {"project_ids" : project_ids,"batch_ids" : batch_ids, "restricted" : restricted}
 
 
 
