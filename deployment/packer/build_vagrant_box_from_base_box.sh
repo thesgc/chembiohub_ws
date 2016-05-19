9908506ae1cf8187db62bc130ad49c271b8055d2
Strets123 <strets123@gmail.com>
2016-05-19 14:01:35 +0100

    base box

--- deployment/packer/build_vagrant_box_from_base_box.sh
+++ deployment/packer/build_vagrant_box_from_base_box.sh
@@ -1,5 +1,6 @@
 cd /srv/chembiohub
-
+git pull
+git submodule foreach git pull origin master
 bash -c "bash scripts/setup_database_and_index.sh dev vagrant"
 bash -c "bash scripts/run_bower.sh dev Ubuntu"
 
