aed292538e4efec6252791bb345a35e878eec571
chembiohub <you@example.com>
2016-05-20 20:55:10 +0100

    db index

--- scripts/setup_database_and_index.sh
+++ scripts/setup_database_and_index.sh
@@ -3,6 +3,9 @@ set -e
 export ENV_NAME="$1"
 OLD_PATH="$PATH"
 mkdir -p /home/$2/anaconda2/envs/$ENV_NAME/var/postgressocket/
+sudo service supervisor restart
+
+
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl restart all
