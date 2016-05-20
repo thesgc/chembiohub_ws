147f5a767cf82f3d1617b2dee71bef871fc3f816
strets123 <strets123@gmail.com>
2016-05-20 17:20:56 +0100

    Update install_ubuntu_dependencies.sh

--- scripts/install_ubuntu_dependencies.sh
+++ scripts/install_ubuntu_dependencies.sh
@@ -1,7 +1,7 @@
 sudo apt-get install -y openjdk-7-jre-headless
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
-sudo add-apt-repository ppa:chris-lea/node.js 
+sudo add-apt-repository ppa:chris-lea/node.js -y
 sudo apt-get update && sudo apt-get install elasticsearch nodejs
 sudo update-rc.d elasticsearch defaults 95 10
 sudo service elasticsearch start
