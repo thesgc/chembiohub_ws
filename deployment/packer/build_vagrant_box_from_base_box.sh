cba22c6805fe6f290d8c96718c8f1b0d1225f3d9
chembiohub <you@example.com>
2016-05-20 20:04:32 +0100

    script tweaks

--- deployment/packer/build_vagrant_box_from_base_box.sh
+++ deployment/packer/build_vagrant_box_from_base_box.sh
@@ -1,9 +1,7 @@
+set -e
 cd /srv/chembiohub
 git pull
 git submodule foreach git pull origin master
-sudo chown -R vagrant /home/vagrant/tmp/
-bash -c "bash scripts/setup_database_and_index.sh dev vagrant"
 bash -c "bash scripts/run_bower.sh dev Ubuntu"
-
 
 
--- scripts/setup_database_and_index.sh
+++ scripts/setup_database_and_index.sh
@@ -2,10 +2,12 @@ set -e
  #Start postgres in foreground
 export ENV_NAME="$1"
 OLD_PATH="$PATH"
-
+mkdir -p /home/$2/anaconda2/envs/$ENV_NAME/var/postgressocket/
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl restart all
+
+printf "sleeping"
 
 sleep 10
 #source activate $ENV_NAME
@@ -14,8 +16,8 @@ export CONDA_ENV_PATH=/home/$2/anaconda2/envs/$ENV_NAME
 export PATH=$CONDA_ENV_PATH/bin:$OLD_PATH
 
 
-$CONDA_ENV_PATH/bin/psql  -h $CONDA_ENV_PATH/var/postgressocket/ -c "create extension if not exists hstore;create extension if not exists  rdkit;" template1
-$CONDA_ENV_PATH/bin/createdb -h $CONDA_ENV_PATH/var/postgressocket/ ${ENV_NAME}_db -T template1
+$CONDA_ENV_PATH/bin/psql  -h $CONDA_ENV_PATH/var/postgressocket -c "create extension if not exists hstore;create extension if not exists  rdkit;" template1
+$CONDA_ENV_PATH/bin/createdb -h $CONDA_ENV_PATH/var/postgressocket ${ENV_NAME}_db -T template1
 
 $CONDA_ENV_PATH/bin/python deployment/generate_secret_settings.py > deployment/settings/secret.py
 $CONDA_ENV_PATH/bin/python manage.py migrate
