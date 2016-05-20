0c12b34031603e6e1a737cf9c786cf7dfbe1d3ea
strets123 <strets123@gmail.com>
2016-05-20 17:46:33 +0100

    Update install_ubuntu_dependencies.sh

--- scripts/install_ubuntu_dependencies.sh
+++ scripts/install_ubuntu_dependencies.sh
@@ -1,3 +1,5 @@
+set -e
+
 sudo apt-get install -y openjdk-7-jre-headless
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
