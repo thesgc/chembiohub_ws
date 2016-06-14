4ab46148e225015e9b93e3c2b4ad2aed02934e5a
Strets123 <strets123@gmail.com>
2016-06-14 20:09:44 +0100

    vagrant stuff and fixes to back end

--- vagrant/vagrant_provision.sh
+++ vagrant/vagrant_provision.sh
@@ -1,1 +1,4 @@
-ln -s /srv/chembiohub /home/vagrant/syw/code
+#ln -s /srv/chembiohub /home/vagrant/syw/code
+cd /srv/chembiohub
+bash /srv/chembiohub/scripts/setup_database_and_index.sh dev vagrant
+echo "LOGIN_REDIRECT_URL = '/#/projects/list'" >> /srv/chembiohub/deployment/settings/secret.py
